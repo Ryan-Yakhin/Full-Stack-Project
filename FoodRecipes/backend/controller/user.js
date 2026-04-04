04f581398972a31912a81559d86cb6ae33727921
RyanYakhin <ryanyakhin4@gmail.com>
2026-04-04 21:03:07 +0700

    got done with user login

--- FoodRecipes/backend/controller/user.js
+++ FoodRecipes/backend/controller/user.js
@@ -9,18 +9,33 @@ const userSingUp = async (req, res) => {
     }
     let user = await User.findOne({ email });
     if(user){
-        return res.status(400).json({ message: "User already exists" });
+        return res.status(400).json({ error: "Email already exists" });
     }
     const hashPwd = await bcrypt.hash(password, 10);
     const newUser = await User.create({ 
         email, password: hashPwd 
     });
     let token = jwt.sign({ email, id: newUser._id}, process.env.SECRET_KEY);
-    return res.status(200).json({ token, newUser });
+    return res.status(200).json({ token, user:newUser });
 }
 
-const userLogin = async (req, res) => {}
+const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({ message: "Email and password are required" });
+    }
+    let user = await User.findOne({email});
+    if(user && await bcrypt.compare(password, user.password)){
+        let token = jwt.sign({ email, id: user._id}, process.env.SECRET_KEY);
+        return res.status(200).json({ token, user });
+    }else{
+        return res.status(400).json({ error: "Invalid credentials" });
+    }
+}
 
-const getUser = async (req, res) => {}
+const getUser = async (req, res) => {
+    const user= await User.findById(req.params.id);
+    res.json({ email: user.email});
+}
 
 module.exports = { userSingUp, userLogin, getUser }
---
+++ FoodRecipes/frontend/food-blog/src/components/InputForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios';
+
+export default function InputForm ({setIsOpen}) {
+
+  const[email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isSignUp, setIsSignUp] = React.useState(false); 
+  const [error, setError] = React.useState("");
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let endpoint = (isSignUp) ? "singUp" : "login";
+    await axios.post(`http://localhost:5000/${endpoint}`, { email, password })
+    .then((res) => {
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      setIsOpen();
+    })
+    .catch(data => setError(data.response?.data?.error)) 
+
+  }
+
+  return (
+    <form className="form" onSubmit={handleOnSubmit}>
+        <div className='form-control'>
+            <label>Email</label>
+            <input type="email" className='input' onChange={(e)=>setEmail(e.target.value)} required></input>
+        </div>
+        <div className='form-control'>
+            <label>Password</label>
+            <input type="password" className='password' onChange={(e)=>setPassword(e.target.value)} required></input>
+        </div>
+        <button type="submit">{(isSignUp) ? "Sign Up" : "Login"}</button><br />
+        {(error != "") && <h6 className='error'>{error}</h6>} <br />
+        <p onClick={()=>setIsSignUp(prev => !prev)}>{(isSignUp) ? "Already have an account?": "Create new account"}</p>
+    </form>
+  )
+}
+
