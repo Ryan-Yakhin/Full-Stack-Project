f431ed8d637fec1422bd41f737549407b2b6c9e8
RyanYakhin <ryanyakhin4@gmail.com>
2026-06-23 01:07:31 +0700

    server.js update

--- Skuyy/backend/server.js
+++ Skuyy/backend/server.js
@@ -12,7 +12,10 @@ app.use(
   
   cors(
     {
-      origin:"https://full-stack-project-zr3y-eufqre7we-ryan-yakhin.vercel.app",
+      origin:[
+        "http://localhost:5173",
+        "https://full-stack-project-zr3y.vercel.app"
+      ],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"] 
