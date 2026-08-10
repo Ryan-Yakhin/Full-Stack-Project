4f9c96c4543457b544822c7661cf1550128cc3f8
RyanYakhin <ryanyakhin4@gmail.com>
2026-08-10 10:54:10 +0700

    connect frontend and backend

---
+++ TaskFlow/server/src/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("✅ MongoDB Connected");
+
+    }
+
+    catch (error) {
+
+        console.error("MongoDB Error");
+
+        console.error(error.message);
+
+        process.exit(1);
+
+    }
+
+}
